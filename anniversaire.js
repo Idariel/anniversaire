6df98b6caac0237b5ce3cb8c924f803d109ac95b
admin <isalechat@yahoo.com>
2018-01-13 01:42:05 +0100

    HTML et CSS OK - JS à faire

--- anniversaire.js
+++ anniversaire.js
@@ -1,1 +1,14 @@
+$body = $('body');
+$header = $('header');
+$h1 = $('h1');
+$section = $('section');
+$article = $('article');
+$footer = $('footer');
 
+/*  $header.hide();
+  $section.hide();
+  $article.hide();
+  $footer.hide();
+
+  $h1.fadeIn(1000);
+*/
